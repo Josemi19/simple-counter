733ff568c6dd49d49567714e5b9b619da4798b14
Jose Morrone <josemmorrone@gmail.com>
2022-03-22 13:33:12 -0400

    [ADD] Ejercicio resuelto, falta el icono del reloj

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,46 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
+import PropsType from "prop-types";
 
-//import your own components
-import Home from "./component/home.jsx";
+function Counter(props) {
+	return (
+		<>
+			<div className="reloj">
+				<div className="clock">
+					<i className="fa-regular fa-clock"></i>
+				</div>
+				<div className="fourth">{props.fourth % 10}</div>
+				<div className="third">{props.third % 10}</div>
+				<div className="second">{props.second % 10}</div>
+				<div className="first">{props.first % 10}</div>
+			</div>
+		</>
+	);
+}
+Counter.propsType = {
+	first: PropsType.number,
+	second: PropsType.number,
+	third: PropsType.number,
+	fourth: PropsType.number,
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let seconds = 0;
+setInterval(() => {
+	let firstDigit = Math.floor(seconds / 1);
+	let secondDigit = Math.floor(seconds / 10);
+	let thirdDigit = Math.floor(seconds / 100);
+	let fourthDigit = Math.floor(seconds / 1000);
+	seconds++;
+	//render your react application
+	ReactDOM.render(
+		<Counter
+			first={firstDigit}
+			second={secondDigit}
+			third={thirdDigit}
+			fourth={fourthDigit}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
